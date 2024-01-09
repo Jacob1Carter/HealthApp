a164a7638b3acf2ce7c146ea041f5e995cc4ab8e
Jacob1Carter <jacobcarter@exe-coll.ac.uk>
2024-01-09 12:08:21 +0000

    details page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -12,14 +13,34 @@ def landing():
 def results():
     search = request.args.get('query')
 
-    #yeaaaaaaaaaaaaaaaaaaaaaaaaa
+    url = 'https://trackapi.nutritionix.com/v2/search/instant/?query=' + search
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'x-app-id': 'e8aee087',
+        'x-app-key': 'ee3fc88b259cf08a43ac94f8dd1afb62'
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-    return render_template("results.html", search=search)
+    return render_template("results.html", search=search, data=data)
 
 
 @app.route("/details/<item>")
 def details(item):
-    return render_template("details.html", item=item)
+    url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+    headers = {
+        "Content-Type": "application/json",
+        "x-app-id": 'e8aee087',
+        "x-app-key": 'ee3fc88b259cf08a43ac94f8dd1afb62'
+    }
+    query_data = {
+        "query": item
+    }
+    response = requests.post(url, headers=headers, json=query_data)
+    data = response.json()
+    food_item = data["foods"][0]
+
+    return render_template("details.html", item=food_item)
 
 
 if __name__ == "__main__":
